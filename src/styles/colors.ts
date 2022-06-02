0843141a8059b9ef7d71168e22ba94c002ccfa56
Davi Alcântara <dxvialcantara@gmail.com>
2022-06-01 22:08:13 -0300

    style: add custom colors provider

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+import { green, violet } from '@radix-ui/colors';
+
+export const primaryGradient =
+  'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)';
+
+export const secondaryGradient =
+  'linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%)';
+
+export const tertiaryGradient = `linear-gradient(90deg, ${violet.violet11} 0%, ${green.green10} 100%)`;
