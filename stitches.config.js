25f6761b7c0ba42f983ceb6d1e0c0a10256cd6ac
Davi Alcântara <dxvialcantara@gmail.com>
2022-06-08 11:48:08 -0300

    style: add stitches styles config

--- stitches.config.js
+++ stitches.config.js
@@ -1,12 +1,4 @@
-import {
-  blue,
-  gray,
-  green,
-  mintA,
-  purpleA,
-  red,
-  slateA,
-} from '@radix-ui/colors';
+import { mauve, mauveDark } from '@radix-ui/colors';
 import { createStitches, globalCss } from '@stitches/react';
 import {
   primaryGradient,
@@ -15,9 +7,13 @@ import {
 } from './src/styles/colors';
 
 export const globalStyles = globalCss({
-  '*': { padding: 0, margin: 0, boxSizing: 'border-box' },
+  '*': {
+    padding: 0,
+    margin: 0,
+    boxSizing: 'border-box',
+  },
   'body, input, textarea, select, button': {
-    font: "400 1rem 'Inter', apple-system, sans-serif",
+    font: "400 1rem 'Noto Sans', apple-system, sans-serif",
     fontSmooth: 'antialiased',
   },
   button: {
@@ -34,9 +30,6 @@ export const globalStyles = globalCss({
     pointerEvents: 'none',
     userSelect: 'none',
   },
-  body: {
-    transition: 'all 0.3s ease',
-  },
   html: {
     '@media (max-width: 1080px)': {
       fontSize: '93.75%',
@@ -50,15 +43,14 @@ export const globalStyles = globalCss({
 export const { styled, css, createTheme, keyframes } = createStitches({
   theme: {
     colors: {
-      ...mintA,
-      ...purpleA,
-      ...slateA,
-      ...gray,
-      ...blue,
-      ...red,
-      ...green,
-      primary: gray.gray5,
-      secondary: gray.gray12,
+      primary: mauve.mauve7,
+      secondary: mauveDark.mauve1,
+
+      lightGray: mauve.mauve5,
+      baseGray: mauve.mauve9,
+      darkGray: mauve.mauve11,
+      darkerGray: mauveDark.mauve2,
+
       primaryGradient,
       secondaryGradient,
       tertiaryGradient,
@@ -68,38 +60,50 @@ export const { styled, css, createTheme, keyframes } = createStitches({
     },
     fontSizes: {
       1: '12px',
-      2: '13px',
-      3: '15px',
+      2: '14px',
+      3: '16px',
+      4: '18px',
+      5: '20px',
     },
     space: {
       1: '4px',
       2: '8px',
-      3: '16px',
-      '3-middle': '18px',
-      4: '32px',
-      5: '64px',
-      6: '128px',
+      3: '10px',
+      4: '12px',
+      5: '14px',
+      6: '16px',
+      7: '18px',
+      8: '20px',
+      9: '24px',
+      10: '32px',
+      11: '48px',
+      12: '56px',
+      13: '64px',
     },
     sizes: {
       1: '4px',
       2: '8px',
-      3: '16px',
-      '3-middle': '18px',
-      4: '32px',
-      5: '64px',
-      6: '128px',
+      3: '10px',
+      4: '12px',
+      5: '14px',
+      6: '16px',
+      7: '18px',
+      8: '20px',
+      9: '24px',
+      10: '32px',
+      11: '48px',
+      12: '56px',
+      13: '64px',
       md: '768px',
     },
     radii: {
       1: '2px',
       2: '4px',
-      3: '8px',
+      3: '6px',
+      4: '8px',
+      5: '10px',
+      6: '12px',
       round: '9999px',
-    },
-    media: {
-      bp1: '(min-width: 640px)',
-      bp2: '(min-width: 768px)',
-      bp3: '(min-width: 1024px)',
     },
     fontWeights: {},
     lineHeights: {},
@@ -109,15 +113,31 @@ export const { styled, css, createTheme, keyframes } = createStitches({
     shadows: {},
     zIndices: {},
     transitions: {
-      colors: '0.15s ease',
+      base: '0.15s ease',
     },
+  },
+  media: {
+    bp1: '(min-width: 640px)',
+    bp2: '(min-width: 768px)',
+    bp3: '(min-width: 1024px)',
+  },
+  utils: {
+    size: value => ({
+      width: value,
+      height: value,
+    }),
   },
 });
 
-export const darkTheme = createTheme('dark-theme', {
+export const lightTheme = createTheme('light-theme', {
   colors: {
-    primary: gray.gray12,
-    secondary: green.green1,
+    primary: mauveDark.mauve1,
+    secondary: mauveDark.mauve12,
+
+    lightGray: mauveDark.mauve5,
+    baseGray: mauveDark.mauve8,
+    darkGray: mauveDark.mauve10,
+    darkerGray: mauve.mauve3,
   },
 });
 
@@ -128,21 +148,35 @@ export const Box = styled('div', {
       md: {
         maxWidth: '$md',
         margin: '0 auto',
-        padding: '$3',
       },
     },
+  },
+  defaultVariants: {
+    limit: 'md',
   },
 });
 
 export const Text = styled('p', {
   color: '$primary',
+  lineHeight: 1.8,
+  variants: {
+    type: {
+      paragraph: {
+        fontSize: '$3',
+      },
+      title: {
+        fontWeight: 500,
+        fontSize: '$4',
+      },
+    },
+  },
 });
 
 export const Container = styled('div', {
   minWidth: '100vw',
   minHeight: '100vh',
   background: '$secondary',
-  transition: '$colors',
+  transition: '$base',
 });
 
 export const Icon = (iconElement, css) => {
@@ -151,3 +185,5 @@ export const Icon = (iconElement, css) => {
   });
   return Icon;
 };
+
+export const standartCubicBezier = 'cubic-bezier(.69,.13,.37,.89)';
