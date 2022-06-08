2c8865caa99bed96a367e08dcc8996f027ff70f7
Davi Alcântara <dxvialcantara@gmail.com>
2022-06-08 14:03:01 -0300

    feat: add prismic providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { PrismicPreview } from '@prismicio/next';
+import { PrismicProvider } from '@prismicio/react';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
+import Link from 'next/link';
+import React from 'react';
 import { Container, globalStyles, lightTheme } from '../../stitches.config';
 import { Header } from '../components/Header';
+import { linkResolver, repositoryName } from '../services/prismic';
 
 globalStyles();
 
@@ -15,10 +20,21 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         dark: 'dark',
       }}
     >
-      <Container>
-        <Header />
-        <Component {...pageProps} />
-      </Container>
+      <PrismicProvider
+        linkResolver={linkResolver}
+        internalLinkComponent={({ href, children, ...props }) => (
+          <Link href={href}>
+            <a {...props}>{children}</a>
+          </Link>
+        )}
+      >
+        <PrismicPreview repositoryName={repositoryName}>
+          <Container>
+            <Header />
+            <Component {...pageProps} />
+          </Container>
+        </PrismicPreview>
+      </PrismicProvider>
     </ThemeProvider>
   );
 };
