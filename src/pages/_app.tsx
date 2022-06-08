c8605a745a45b28bc91c51fdbb58ff4f5d78b686
Davi Alcântara <dxvialcantara@gmail.com>
2022-06-08 11:49:26 -0300

    feat: add theme switcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
-import type { AppProps } from 'next/app';
-import { globalStyles } from '../../stitches.config.js';
-import { Header } from '../components/Header/index';
-import { ThemeProvider } from '../providers/themeProvider';
+import { ThemeProvider } from 'next-themes';
+import { AppProps } from 'next/app';
+import { Container, globalStyles, lightTheme } from '../../stitches.config';
+import { Header } from '../components/Header';
 
 globalStyles();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
-      <Header />
-      <Component {...pageProps} />
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      value={{
+        light: lightTheme.className,
+        dark: 'dark',
+      }}
+    >
+      <Container>
+        <Header />
+        <Component {...pageProps} />
+      </Container>
     </ThemeProvider>
   );
 };
