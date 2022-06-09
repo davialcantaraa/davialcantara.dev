eb760aa15622ab35c5b838bfcdfdc70544a18789
Davi Alcântara <dxvialcantara@gmail.com>
2022-06-09 16:15:00 -0300

    types(post): change publication date type

--- src/types/posts.d.ts
+++ src/types/posts.d.ts
@@ -1,7 +1,13 @@
+import * as prismicT from '@prismicio/types';
+
 export interface IPost {
   uid: string;
   title: string;
   subtitle: string;
-  content: string;
-  publicationDate: Date;
+  content: prismicT.RichTextField | null | undefined;
+  publicationDate:
+    | `${number}-${number}-${number}`
+    | `${number}-${number}-${number}T${number}:${number}:${number}+${number}`
+    | null
+    | undefined;
 }
