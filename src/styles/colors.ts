34dcfba47fac25068d48e3bd4ad08d4f5b8e5c30
Davi Alcântara <dxvialcantara@gmail.com>
2022-06-09 16:15:22 -0300

    feat: add colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,15 +1,21 @@
-import { green, violet } from '@radix-ui/colors';
+import {
+  blue,
+  green,
+  mint,
+  pink,
+  purple,
+  violet,
+  yellow,
+} from '@radix-ui/colors';
 
-export const primaryGradient =
-  'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)';
+export const primaryGradient = `linear-gradient(43deg, ${blue.blue10} 0%, ${pink.pink9} 46%, ${yellow.yellow10} 100%)`;
 
-export const secondaryGradient =
-  'linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%)';
+export const secondaryGradient = `linear-gradient(135deg, ${mint.mint8} 0%, ${purple.purple9} 100%)`;
 
-export const tertiaryGradient = `linear-gradient(90deg, ${violet.violet11} 0%, ${green.green10} 100%)`;
+export const tertiaryGradient = `linear-gradient(43deg, ${violet.violet11} 0%, ${green.green10} 100%)`;
 
 export const primaryGradientA =
-  'linear-gradient(43deg, rgba(65, 88, 208, 0.1) 0%, rgba(200, 80, 192, 0.1) 46%, rgba(255, 204, 112, 0.1) 100%)';
+  'linear-gradient(43deg, hsla(208, 100%, 47.2%, 0.1) 0%, hsla(322, 99.7%, 39.3%, 0.1) 46%, hsla(50, 100%, 48.4%, 0.1) 100%)';
 
 export const primaryRadialA =
-  'radial-gradient(circle 500px at 0% 0%, hsla(250, 99.8%, 28.4%, 0.25) 0%, transparent), radial-gradient(circle 500px at 100% 100%, hsla(272, 99.8%, 29.7%, 0.25) 0%, transparent)';
+  'radial-gradient(circle 500px at 0% 0%, hsla(272, 99.6%, 34.0%, 0.18) 0%, transparent), radial-gradient(circle 500px at 100% 100%, hsla(167, 99.7%, 37.4%, 0.18) 0%, transparent)';
