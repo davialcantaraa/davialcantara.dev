9564f1200f4a5286cdd2f638b9b7099896e308d0
Davi Alcântara <dxvialcantara@gmail.com>
2022-06-10 11:37:57 -0300

    types: add nft types

---
+++ src/types/nfts.d.ts
@@ -0,0 +1,68 @@
+export interface INftCollection {
+  collectionName: string;
+  openseaUrl: string;
+  nfts: INft[];
+}
+
+export interface INft {
+  title: string;
+  description: string;
+  contract: string;
+  image: string;
+  id: number;
+  openseaUrl: string;
+}
+
+export interface Contract {
+  address: string;
+}
+
+export interface TokenMetadata {
+  tokenType: string;
+}
+
+export interface Id {
+  tokenId: string;
+  tokenMetadata: TokenMetadata;
+}
+
+export interface TokenUri {
+  raw: string;
+  gateway: string;
+}
+
+export interface Medium {
+  raw: string;
+  gateway: string;
+}
+
+export interface Attribute {
+  value: string;
+  trait_type: string;
+}
+
+export interface Metadata {
+  name: string;
+  description: string;
+  dna: number;
+  image: string;
+  external_url: string;
+  attributes: Attribute[];
+}
+
+export interface OwnedNft {
+  contract: Contract;
+  id: Id;
+  title: string;
+  description: string;
+  tokenUri: TokenUri;
+  media: Medium[];
+  metadata: Metadata;
+  timeLastUpdated: Date;
+}
+
+export interface IReceivedNft {
+  ownedNfts: OwnedNft[];
+  totalCount: number;
+  blockHash: string;
+}
